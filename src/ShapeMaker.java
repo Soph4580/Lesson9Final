b7c76c3de7581d610e7729b35022fd8965e12ca6
soph4580 <soph4580@ISS147D8-26.scdsb.pri>
2014-11-17 09:19:41 -0500

    helpew24

--- src/ShapeMaker.java
+++ src/ShapeMaker.java
@@ -16,8 +16,30 @@ public class ShapeMaker {
         shape3= makeOneShapeFromAnother (rect, "wheel");
       
       System.out.println("\nRectangle Area: " + rect.area() + "\nCircle Area: " + shape1.area() + "\nRectangle Area: " + shape2.area() + "\nWheel Area: " + shape3.area());
-      
-      
-    }
+      }
+    
+    static private ShapeMaker makeOneShapeFromAnother (Shape inShape, String type){
+        Shape outShape;
+        //Declare outShape
+        double area, radius, width, length;
+        double x = inShape.getxpos();
+        double y = inShape.getypos();
+        
+        area = inShape.area();
+        if(type.equals("Circle")){
+            radius = Math.sqrt(area/ Math.PI);
+            outShape = new Circle (x, y, radius);
+            //Assign a circle
+        }
+        else if (type.equals("Rectangle")){
+            width = length = Math.sqrt(area);
+            outShape = new Rect (x, y, radius, 6);
+            
+            //Assign a wheel
+        }
+         return outShape;   
+        }
+        
+    } 
     
 }
