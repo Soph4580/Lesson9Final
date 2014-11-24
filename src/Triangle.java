1cba8b2ce04b84b22c728a5c07b5096f92bc5936
soph4580 <soph4580@ISS147D8-26.scdsb.pri>
2014-11-24 09:14:57 -0500

    triangle edit

--- src/Triangle.java
+++ src/Triangle.java
@@ -10,16 +10,21 @@ public class Triangle extends AbstractShape {
    
     @Override
     public double area() {
-       
+       return Math.abs((xpos*ypos2-xpos2*ypos)+(xpos2*ypos3-xpos3*ypos2)+(xpos3*ypos-xpos*ypos3))/2;
     }
 
     public void draw(Pen p) {
        
     }
 
-    
-    public void stretchBy(double factor) {
+    public void move(double xloc, double yloc){
         
+    }
+    public void stretchBy(double factor) {
+        xpos2=(xpos2-xpos)*factor+xpos2;
+        xpos3=(xpos3-xpos)*factor+xpos3;
+        ypos2=(ypos2-ypos)*factor+ypos2;
+        ypos3=(ypos3=ypos)*factor+ypos3;
     }
 
     @Override
@@ -29,7 +34,7 @@ public class Triangle extends AbstractShape {
 
    
     public double perimeter() {
-       
+       double p = Math.sqrt((xpos-xpos2)*(xpos-xpos2)+(ypos-ypos2)*(ypos-ypos2));
     }
     
 }
